4ffda4e28deea87330ff486ca26c48503c255347
nannavaremayur967-collab <nannavaremayur967@gmail.com>
2026-05-02 09:46:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/react-swc'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 export default defineConfig({
   base: '/i-love-pdf-toolkitt/',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
 })
