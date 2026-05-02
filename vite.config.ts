d0034c361a64d8ffeb5ef63b2d12af9e26d4d8f8
nannavaremayur967-collab <nannavaremayur967@gmail.com>
2026-05-02 10:04:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc'
 
 export default defineConfig({
   base: '/i-love-pdf-toolkitt/',
-  plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
